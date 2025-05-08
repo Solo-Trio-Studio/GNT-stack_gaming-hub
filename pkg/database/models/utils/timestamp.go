f3ffef837afbbb12e03e0710530bde69f2f92c59
ietzel <67884277+ietzel@users.noreply.github.com>
2025-05-08 17:42:41 -0400

    Deleted PostreSQL & NATS files

--- pkg/database/models/utils/timestamp.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "time"
-
-type Timestamp struct {
-	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
-	UpdatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
-}
